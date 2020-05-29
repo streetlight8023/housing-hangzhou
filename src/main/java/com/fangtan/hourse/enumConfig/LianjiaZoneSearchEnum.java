76dad453d39391aa997848323ec78e2644705ed6
樊政启 <597771674@qq.com>
2020-05-29 21:22:19 +0800

    fx

--- src/main/java/com/fangtan/hourse/enumConfig/LianjiaZoneSearchEnum.java
+++ src/main/java/com/fangtan/hourse/enumConfig/LianjiaZoneSearchEnum.java
@@ -5,8 +5,8 @@ public enum LianjiaZoneSearchEnum {
 
     SHANG_CHENG("shangcheng", "上城"),
     XIA_CHENG("xiacheng", "下城"),
-    XI_HU("xihu", "西湖"),
-    BIN_JIANG("binjiang", "滨江"),
+//    XI_HU("xihu", "西湖"),
+//    BIN_JIANG("binjiang", "滨江"),
     GONG_SHU("gongshu", "拱墅"),
     QIAN_JIANG("qiantangxinqu", "钱江新区"),
     DA_JIANG_DONG("dajiangdong1", "大江东"),
