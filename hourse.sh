4fe94137a624a01cc4f4989b835d467cf79fe4cc
樊政启 <597771674@qq.com>
2020-05-31 07:13:18 +0800

    fx

--- hourse.sh
+++ hourse.sh
@@ -2,4 +2,5 @@ Hid=$(jps | grep hourse | awk '{print $1}')
 echo $Hid
   kill -9 $Hid
 rm ./nohup.out;
+mvn clean install
 nohup java -jar -Dfile.encoding=UTF-8 -Xms512m -Xmx1024m -Xmn512m -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:/home/ishangjie/gclog/hourse/gc.log ./target/hourse-1.0-SNAPSHOT.jar  --spring.profiles.active=local &> ./nohup.out &
