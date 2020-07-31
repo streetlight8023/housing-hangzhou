ffa7f34fb574e997e697144f185efc05f57c9cd0
樊政启 <597771674@qq.com>
2020-07-31 09:14:04 +0800

    fx

--- src/main/java/com/fangtan/hourse/web/LianjiaHourseController.java
+++ src/main/java/com/fangtan/hourse/web/LianjiaHourseController.java
@@ -54,7 +54,7 @@ public class LianjiaHourseController {
         bizlogger.info("执行 校验数据 是否存入 数据库 定时任务");
         LianjiaZoneSearchEnum[] values = LianjiaZoneSearchEnum.values();
         for (LianjiaZoneSearchEnum value : values) {
-//            lianjiaService.deleteZoneCache(value.getCode());
+            lianjiaService.deleteZoneCache(value.getCode());
             lianjiaService.parseLianjiaDate(value.getCode());
 //            lianjiaService.parseLianjiaDate("gongshu");
         }
